c9149c78063c56d0e25308447f58c035d57b03b4
mark <markternu@gmail.com>
2024-07-10 18:15:23 +0800

    Up

--- transmission/transmission.sh
+++ transmission/transmission.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-# 安装 Transmission
-sudo yum update -y && sudo yum install epel-release -y && \
-sudo yum install wget vim net-tools  zsh git screen zip aria2 jq unzip -y && \
-sudo yum install -y transmission-daemon && \
+# 读取用户输入
+read -p "Please input URL/IP Addr: " value_url
+read -p "Please input password: " value_psw
 
-# 停止 Transmission 以便修改配置文件
-systemctl start transmission-daemon.service && \
-systemctl stop transmission-daemon.service && \
+# 处理输入参数
+value_url_str="$value_url,::1"
+value_psw_str="$value_psw"
 
-# 修改配置文件
+# 安装 Transmission 和必要的工具
+sudo yum install epel-release -y
+sudo yum update -y
+sudo yum -y install transmission transmission-daemon jq vim
+
+# 启动并停止 Transmission 以生成默认配置文件
+sudo systemctl start transmission-daemon.service
+sudo systemctl stop transmission-daemon.service
+
+# 定义配置文件路径
 CONFIG_FILE="/var/lib/transmission/.config/transmission-daemon/settings.json"
 
-sudo jq '. 
-  | .["rpc-authentication-required"] = true 
-  | .["rpc-password"] = "666888a" 
-  | .["rpc-username"] = "opengl" 
-  | .["rpc-whitelist-enabled"] = true 
-  | .["rpc-whitelist"] = "127.0.0.1"
-  | .["download-queue-enabled"] = false
-  | .["download-queue-size"] = 500 
-  | .["speed-limit-up"] = 0 
-  | .["speed-limit-up-enabled"] = true' $CONFIG_FILE | sudo tee $CONFIG_FILE
+# 修改配置文件
+sudo jq --arg url "$value_url_str" --arg psw "$value_psw_str" '
+.["rpc-authentication-required"] = true |
+.["rpc-password"] = $psw |
+.["rpc-username"] = "opengl" |
+.["rpc-whitelist-enabled"] = true |
+.["rpc-whitelist"] = $url |
+.["download-queue-enabled"] = true |
+.["download-queue-size"] = 500 |
+.["speed-limit-up"] = 0 |
+.["speed-limit-up-enabled"] = true' $CONFIG_FILE > /tmp/settings.json && sudo mv /tmp/settings.json $CONFIG_FILE
 
 # 启动 Transmission
-systemctl start transmission-daemon.service  && \
+sudo systemctl start transmission-daemon.service
 
 # 获取并打印 rpc-password 和 rpc-username 的值
 RPC_PASSWORD=$(jq -r '."rpc-password"' $CONFIG_FILE)
